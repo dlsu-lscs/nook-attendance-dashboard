9d5535632bdd8e7242003b7107c4af881622e013
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-06-02 00:29:39 +0800

    feat: added navbar

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,15 @@
+export const Navbar = () => {
+  return (
+    <>
+      <div className="flex justify-between p-4">
+        <div className="flex items-center">
+          <img src="/lscs.png" alt="lscsLogo" className="w-28 h-28" />
+          <div className="flex flex-col justify-center text-3xl font-bold">
+            <h1>Research and Development</h1>
+            <h1 className="text-lg">39th La Salle Computer Society</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
